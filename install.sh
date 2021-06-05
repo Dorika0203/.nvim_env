c15a96b5192d4d7d02c4a09c8b2d8506fe6b690b
Dorika0203 <doyeol0203@gmail.com>
2021-06-06 03:37:33 +0900

    adfasld

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Get current pwd
+
+dir=$(pwd)
+echo $dir
+
 # Link directory create into .config/
-ln -s ~/nvim ~/.config
+ln -s $dir ~/.config/nvim
 
 # Install vim-plug (for linux)
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
